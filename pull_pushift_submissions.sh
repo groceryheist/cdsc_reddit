d05da6441f85b938b65bd47655602d2569f091b6
Nate E TeBlunthuis <nathante@n2232.hyak.local>
2020-07-03 10:40:43 -0700

    Don't clobber old dumps so that we can just download the new ones.

--- pull_pushift_submissions.sh
+++ pull_pushift_submissions.sh
@@ -4,11 +4,11 @@ user_agent='nathante teblunthuis <nathante@uw.edu>'
 output_dir='/gscratch/comdata/raw_data/reddit_dumps/submissions'
 base_url='https://files.pushshift.io/reddit/submissions/'
 
-wget -r --no-parent -A 'RS_20*.bz2' -U $user_agent -P $output_dir -nd $base_url
-wget -r --no-parent -A 'RS_20*.xz' -U $user_agent -P $output_dir -nd $base_url
-wget -r --no-parent -A 'RS_20*.zst' -U $user_agent -P $output_dir -nd $base_url
-wget -r --no-parent -A 'RS_20*.bz2' -U $user_agent -P $output_dir -nd $base_url/old_v1_data
-wget -r --no-parent -A 'RS_20*.xz' -U $user_agent -P $output_dir -nd $base_url/old_v1_data
-wget -r --no-parent -A 'RS_20*.zst' -U $user_agent -P $output_dir -nd $base_url/old_v1_data
+wget -r --no-parent -A 'RS_20*.bz2' -U $user_agent -P $output_dir -nd -nc $base_url
+wget -r --no-parent -A 'RS_20*.xz' -U $user_agent -P $output_dir -nd -nc $base_url
+wget -r --no-parent -A 'RS_20*.zst' -U $user_agent -P $output_dir -nd -nc $base_url
+wget -r --no-parent -A 'RS_20*.bz2' -U $user_agent -P $output_dir -nd -nc $base_url/old_v1_data
+wget -r --no-parent -A 'RS_20*.xz' -U $user_agent -P $output_dir -nd -nc $base_url/old_v1_data
+wget -r --no-parent -A 'RS_20*.zst' -U $user_agent -P $output_dir -nd -nc $base_url/old_v1_data
 
 
